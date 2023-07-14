436b50eeb71714d992a304932d9ec31687ed00db
myrajarenga <myrajarenga1234@gmail.com>
2023-07-14 02:15:58 -0700

    adding country column to atable

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- creating table in holberton database
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN')NOT NULL DEFAULT 'US'
+);
