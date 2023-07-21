f90b2c798dd4d486b785661a6219bf46b2e13cdd
myrajarenga <myrajarenga1234@gmail.com>
2023-07-21 10:33:56 +0000

    writing strings to redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Writing strings to Redis
+"""
+
+
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    def __init__(self):
+        """sore an instance of redis as private"""
+        self._redis = redis.Redis()
+
+        """flush the instance"""
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]):
+        """generate random key using UUID"""
+        key = str(uuid.uuid4())
+
+        """store input in redis using rando keym"""
+        self._redis.set(key, data)
+
+        return key
